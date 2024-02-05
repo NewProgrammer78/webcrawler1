3dbf45c27794a624b127bd8daba1f54e5dd5f0b5
Logan Lochstampfor <loganl@itl-solutions.com>
2024-02-05 12:56:39 -0500

    error found on line 13

---
+++ wbc1.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+co = webdriver.ChromeOptions()
+co.add_argument("--incognito")
+co.add_argument("--window-size=1920,1080")
+driver = webdriver.Chrome(options=co)
+start_url = "https://itl-solutions.com/"
+links = set()
+
+def crawl(start_link):
+    driver.get(start_link)
+    elements = driver.find_elements_by_tag_name("a")
+    urls_to_visit = set()
+    for el in elements:
+        urls_to_visit.add(el.get_attribute('href'))
+    for el in urls_to_visit:
+        if start_url in el and el not in links:
+            links.add(el)
+            crawl(el)
+
+# Call the function with the starting URL
+crawl(start_url)
+
+# Print the collected links
+print("Collected links:")
+for link in links:
+    print(link)
+
+# Close the Chrome driver
+driver.close()
+
+# at the end of script wait for user to supply input, delaying script exit
+raw_input("Press Enter to exit")
